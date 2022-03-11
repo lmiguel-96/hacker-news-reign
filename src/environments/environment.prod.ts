21eee61a2b608ca97348abb1f4d2b88783eb34b5
Luis Labarca <lmiguelab@gmail.com>
2022-03-10 23:52:28 -0400

    fix: attempt to resolve an issue with netlify deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env['npm_package_version'],
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://hn.algolia.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES'],
 };
